81c68133b6a5ae748f0b400bec481cd17988eb5a
Nikhita1504 <136711092+Nikhita1504@users.noreply.github.com>
2025-05-18 17:42:11 +0530

    done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
+import os
 
 app=Flask(__name__)
 model=pickle.load(open('housePred.pkl','rb'))
@@ -34,9 +35,8 @@ def predict():
 
 
 
-if __name__=="__main__":
-    app.run(debug=True)
-
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
 
